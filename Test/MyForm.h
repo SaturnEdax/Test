202a4857b10e7e7f75eaf96eb91e7579dd1030e2
SaturnEdax <zuravlsof@gmail.com>
2025-09-19 21:59:58 +0500

    <Обязательно>

--- Test/MyForm.h
+++ Test/MyForm.h
@@ -35,6 +35,9 @@ namespace Test {
 			}
 		}
 	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::TrackBar^ trackBar1;
 	protected:
 
 	private:
@@ -51,29 +54,69 @@ namespace Test {
 		void InitializeComponent(void)
 		{
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->trackBar1 = (gcnew System::Windows::Forms::TrackBar());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->trackBar1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// panel1
 			// 
+			this->panel1->BackColor = System::Drawing::SystemColors::GradientInactiveCaption;
 			this->panel1->Location = System::Drawing::Point(33, 38);
 			this->panel1->Name = L"panel1";
 			this->panel1->Size = System::Drawing::Size(626, 380);
 			this->panel1->TabIndex = 0;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(33, 441);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(143, 49);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Загрузить файл";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(516, 441);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(143, 49);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Сформировать результат";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// trackBar1
+			// 
+			this->trackBar1->Location = System::Drawing::Point(210, 434);
+			this->trackBar1->Maximum = 500;
+			this->trackBar1->Name = L"trackBar1";
+			this->trackBar1->Size = System::Drawing::Size(267, 56);
+			this->trackBar1->TabIndex = 2;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(700, 515);
+			this->Controls->Add(this->trackBar1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
 			this->Controls->Add(this->panel1);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->trackBar1))->EndInit();
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		gdfgf
 	}
 	};
 }
